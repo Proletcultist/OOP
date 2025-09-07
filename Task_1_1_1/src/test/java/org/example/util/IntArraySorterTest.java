1b17c0914cad54ceec5f7fbf9ff84f3fdcf5473a
Proletcultist <zenin2606@gmail.com>
2025-09-07 15:41:21 +0700

    Added tests for IntArraySorter

---
+++ Task_1_1_1/src/test/java/org/example/util/IntArraySorterTest.java
@@ -0,0 +1,53 @@
+package org.example.util;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class IntArraySorterTest {
+
+  @Test
+  public void testRandomArray() {
+    var arr = IntArraySorter.heapSort(new int[] {4, 5, 1, 3});
+
+    Assertions.assertArrayEquals(arr, new int[] {1, 3, 4, 5});
+  }
+
+  @Test
+  public void testEmptyArray() {
+    var arr = IntArraySorter.heapSort(new int[] {});
+
+    Assertions.assertArrayEquals(arr, new int[] {});
+  }
+
+  @Test
+  public void testSingleElementedArray() {
+    var arr = IntArraySorter.heapSort(new int[] {2});
+
+    Assertions.assertArrayEquals(arr, new int[] {2});
+  }
+
+  @Test
+  public void testSortOfCopy() {
+    var arr = new int[] {1, 7, 3, 9};
+
+    var sortedArr = IntArraySorter.heapSort(Arrays.copyOf(arr, arr.length));
+
+    Assertions.assertArrayEquals(arr, new int[] {1, 7, 3, 9});
+    Assertions.assertArrayEquals(sortedArr, new int[] {1, 3, 7, 9});
+  }
+
+  @Test
+  public void testEqualElements() {
+    var arr = IntArraySorter.heapSort(new int[] {9, 1, 1, 2, 3, 2});
+
+    Assertions.assertArrayEquals(arr, new int[] {1, 1, 2, 2, 3, 9});
+  }
+
+  @Test
+  public void testAlreadySorted() {
+    var arr = IntArraySorter.heapSort(new int[] {1, 2, 3, 4, 5});
+
+    Assertions.assertArrayEquals(arr, new int[] {1, 2, 3, 4, 5});
+  }
+}
