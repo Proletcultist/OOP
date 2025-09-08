79cbcdca9a8a72d223672a809b89aac2c84bc035
Proletcultist <zenin2606@gmail.com>
2025-09-08 10:21:25 +0700

    [Task_1_1_1] Fixed codestyle

--- Task_1_1_1/src/main/java/org/example/util/Heapifier.java
+++ Task_1_1_1/src/main/java/org/example/util/Heapifier.java
@@ -1,49 +1,49 @@
 package org.example.util;
 
-/** Utils for making and maintaining heap on int array */
+/** Utils for making and maintaining heap on int array. */
 public class Heapifier {
 
-  /**
-   * Make heap on existing array
-   *
-   * @param arr - array, on which heap will be built
-   * @param size - size of a heap on array
-   */
-  public static void makeHeap(int[] arr, int size) {
-    // Sift down all nodes, starting from last non-leaf node
-    for (int i = size / 2 - 1; i >= 0; i--) {
-      siftDown(arr, size, i);
-    }
-  }
-
-  /**
-   * Sift down node, restoring invariant of heap
-   *
-   * @param arr - array, pepresenting heap
-   * @param size - size of a heap
-   * @param index - index of node to sift down
-   */
-  public static void siftDown(int[] arr, int size, int index) {
-    int l = index * 2 + 1;
-    int r = index * 2 + 2;
-
-    int largest = index;
-
-    if (l < size && arr[l] > arr[largest]) {
-      largest = l;
+    /**
+     * Make heap on existing array.
+     *
+     * @param arr - array, on which heap will be built
+     * @param size - size of a heap on array
+     */
+    public static void makeHeap(int[] arr, int size) {
+        // Sift down all nodes, starting from last non-leaf node
+        for (int i = size / 2 - 1; i >= 0; i--) {
+            siftDown(arr, size, i);
+        }
     }
 
-    if (r < size && arr[r] > arr[largest]) {
-      largest = r;
+    /**
+     * Sift down node, restoring invariant of heap.
+     *
+     * @param arr - array, pepresenting heap
+     * @param size - size of a heap
+     * @param index - index of node to sift down
+     */
+    public static void siftDown(int[] arr, int size, int index) {
+        int l = index * 2 + 1;
+        int r = index * 2 + 2;
+
+        int largest = index;
+
+        if (l < size && arr[l] > arr[largest]) {
+            largest = l;
+        }
+
+        if (r < size && arr[r] > arr[largest]) {
+            largest = r;
+        }
+
+        if (largest != index) {
+            int tmp = arr[largest];
+
+            arr[largest] = arr[index];
+            arr[index] = tmp;
+
+            siftDown(arr, size, largest);
+        }
     }
-
-    if (largest != index) {
-      int tmp = arr[largest];
-
-      arr[largest] = arr[index];
-      arr[index] = tmp;
-
-      siftDown(arr, size, largest);
-    }
-  }
 }
--- Task_1_1_1/src/test/java/org/example/util/IntArraySorterTest.java
+++ Task_1_1_1/src/test/java/org/example/util/IntArraySorterTest.java
@@ -4,66 +4,68 @@ import java.util.Arrays;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+/** Tests for IntArraySorter class. */
 public class IntArraySorterTest {
 
-  @Test
-  public void testRandomArray() {
-    var arr = IntArraySorter.heapSort(new int[] {4, 5, 1, 3});
+    @Test
+    public void testRandomArray() {
+        var arr = IntArraySorter.heapSort(new int[] {4, 5, 1, 3});
 
-    Assertions.assertArrayEquals(arr, new int[] {1, 3, 4, 5});
-  }
+        Assertions.assertArrayEquals(arr, new int[] {1, 3, 4, 5});
+    }
 
-  @Test
-  public void testEmptyArray() {
-    var arr = IntArraySorter.heapSort(new int[] {});
+    @Test
+    public void testEmptyArray() {
+        var arr = IntArraySorter.heapSort(new int[] {});
 
-    Assertions.assertArrayEquals(arr, new int[] {});
-  }
+        Assertions.assertArrayEquals(arr, new int[] {});
+    }
 
-  @Test
-  public void testSingleElementedArray() {
-    var arr = IntArraySorter.heapSort(new int[] {2});
+    @Test
+    public void testSingleElementedArray() {
+        var arr = IntArraySorter.heapSort(new int[] {2});
 
-    Assertions.assertArrayEquals(arr, new int[] {2});
-  }
+        Assertions.assertArrayEquals(arr, new int[] {2});
+    }
 
-  @Test
-  public void testSortOfCopy() {
-    var arr = new int[] {1, 7, 3, 9};
+    @Test
+    public void testSortOfCopy() {
+        var arr = new int[] {1, 7, 3, 9};
 
-    var sortedArr = IntArraySorter.heapSort(Arrays.copyOf(arr, arr.length));
+        var sortedArr = IntArraySorter.heapSort(Arrays.copyOf(arr, arr.length));
 
-    Assertions.assertArrayEquals(arr, new int[] {1, 7, 3, 9});
-    Assertions.assertArrayEquals(sortedArr, new int[] {1, 3, 7, 9});
-  }
+        Assertions.assertArrayEquals(arr, new int[] {1, 7, 3, 9});
+        Assertions.assertArrayEquals(sortedArr, new int[] {1, 3, 7, 9});
+    }
 
-  @Test
-  public void testEqualElements() {
-    var arr = IntArraySorter.heapSort(new int[] {9, 1, 1, 2, 3, 2});
+    @Test
+    public void testEqualElements() {
+        var arr = IntArraySorter.heapSort(new int[] {9, 1, 1, 2, 3, 2});
 
-    Assertions.assertArrayEquals(arr, new int[] {1, 1, 2, 2, 3, 9});
-  }
+        Assertions.assertArrayEquals(arr, new int[] {1, 1, 2, 2, 3, 9});
+    }
 
-  @Test
-  public void testAlreadySorted() {
-    var arr = IntArraySorter.heapSort(new int[] {1, 2, 3, 4, 5});
+    @Test
+    public void testAlreadySorted() {
+        var arr = IntArraySorter.heapSort(new int[] {1, 2, 3, 4, 5});
 
-    Assertions.assertArrayEquals(arr, new int[] {1, 2, 3, 4, 5});
-  }
+        Assertions.assertArrayEquals(arr, new int[] {1, 2, 3, 4, 5});
+    }
 
-  @Test
-  public void testNegative() {
-    var arr = IntArraySorter.heapSort(new int[] {-12, 0, 9, -100, 92});
+    @Test
+    public void testNegative() {
+        var arr = IntArraySorter.heapSort(new int[] {-12, 0, 9, -100, 92});
 
-    Assertions.assertArrayEquals(arr, new int[] {-100, -12, 0, 9, 92});
-  }
+        Assertions.assertArrayEquals(arr, new int[] {-100, -12, 0, 9, 92});
+    }
 
-  @Test
-  public void testMinMax() {
-    var arr =
-        IntArraySorter.heapSort(new int[] {Integer.MAX_VALUE, 0, 127, 512, -10, Integer.MIN_VALUE});
+    @Test
+    public void testMinMax() {
+        var arr =
+                IntArraySorter.heapSort(
+                        new int[] {Integer.MAX_VALUE, 0, 127, 512, -10, Integer.MIN_VALUE});
 
-    Assertions.assertArrayEquals(
-        arr, new int[] {Integer.MIN_VALUE, -10, 0, 127, 512, Integer.MAX_VALUE});
-  }
+        Assertions.assertArrayEquals(
+                arr, new int[] {Integer.MIN_VALUE, -10, 0, 127, 512, Integer.MAX_VALUE});
+    }
 }
