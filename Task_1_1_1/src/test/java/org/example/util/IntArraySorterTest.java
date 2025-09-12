bd47c645e072a5a3459a05f5ce90cb578be6eeb6
Proletcultist <zenin2606@gmail.com>
2025-09-12 08:35:13 +0700

    Removed returning of reference on sorted list

--- Task_1_1_1/src/test/java/org/example/util/IntArraySorterTest.java
+++ Task_1_1_1/src/test/java/org/example/util/IntArraySorterTest.java
@@ -1,6 +1,5 @@
 package org.example.util;
 
-import java.util.Arrays;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -9,61 +8,56 @@ public class IntArraySorterTest {
 
     @Test
     public void testRandomArray() {
-        var arr = IntArraySorter.heapSort(new int[] {4, 5, 1, 3});
+        var arr = new int[] {4, 5, 1, 3};
+        IntArraySorter.heapSort(arr);
 
         Assertions.assertArrayEquals(arr, new int[] {1, 3, 4, 5});
     }
 
     @Test
     public void testEmptyArray() {
-        var arr = IntArraySorter.heapSort(new int[] {});
+        var arr = new int[] {};
+        IntArraySorter.heapSort(arr);
 
         Assertions.assertArrayEquals(arr, new int[] {});
     }
 
     @Test
     public void testSingleElementedArray() {
-        var arr = IntArraySorter.heapSort(new int[] {2});
+        var arr = new int[] {2};
+        IntArraySorter.heapSort(arr);
 
         Assertions.assertArrayEquals(arr, new int[] {2});
     }
 
     @Test
-    public void testSortOfCopy() {
-        var arr = new int[] {1, 7, 3, 9};
-
-        var sortedArr = IntArraySorter.heapSort(Arrays.copyOf(arr, arr.length));
-
-        Assertions.assertArrayEquals(arr, new int[] {1, 7, 3, 9});
-        Assertions.assertArrayEquals(sortedArr, new int[] {1, 3, 7, 9});
-    }
-
-    @Test
     public void testEqualElements() {
-        var arr = IntArraySorter.heapSort(new int[] {9, 1, 1, 2, 3, 2});
+        var arr = new int[] {9, 1, 1, 2, 3, 2};
+        IntArraySorter.heapSort(arr);
 
         Assertions.assertArrayEquals(arr, new int[] {1, 1, 2, 2, 3, 9});
     }
 
     @Test
     public void testAlreadySorted() {
-        var arr = IntArraySorter.heapSort(new int[] {1, 2, 3, 4, 5});
+        var arr = new int[] {1, 2, 3, 4, 5};
+        IntArraySorter.heapSort(arr);
 
         Assertions.assertArrayEquals(arr, new int[] {1, 2, 3, 4, 5});
     }
 
     @Test
     public void testNegative() {
-        var arr = IntArraySorter.heapSort(new int[] {-12, 0, 9, -100, 92});
+        var arr = new int[] {-12, 0, 9, -100, 92};
+        IntArraySorter.heapSort(arr);
 
         Assertions.assertArrayEquals(arr, new int[] {-100, -12, 0, 9, 92});
     }
 
     @Test
     public void testMinMax() {
-        var arr =
-                IntArraySorter.heapSort(
-                        new int[] {Integer.MAX_VALUE, 0, 127, 512, -10, Integer.MIN_VALUE});
+        var arr = new int[] {Integer.MAX_VALUE, 0, 127, 512, -10, Integer.MIN_VALUE};
+        IntArraySorter.heapSort(arr);
 
         Assertions.assertArrayEquals(
                 arr, new int[] {Integer.MIN_VALUE, -10, 0, 127, 512, Integer.MAX_VALUE});
