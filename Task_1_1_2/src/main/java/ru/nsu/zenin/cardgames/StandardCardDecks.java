618d2584053a6d2d21296c2df6a6ec9a8dddb055
Proletcultist <zenin2606@gmail.com>
2025-09-14 12:31:01 +0700

    Implemented Card, CardDeck and StandardCardDecks classes

---
+++ Task_1_1_2/src/main/java/ru/nsu/zenin/cardgames/StandardCardDecks.java
@@ -0,0 +1,67 @@
+package ru.nsu.zenin.cardgames;
+
+import static ru.nsu.zenin.cardgames.Card.CardRank.*;
+import static ru.nsu.zenin.cardgames.Card.CardSuit.*;
+
+public final class StandardCardDecks {
+
+    private StandardCardDecks() {}
+
+    public static CardDeck fullFrenchSuitedDeck() {
+        return new CardDeck(
+                new Card(CLUBS, TWO),
+                new Card(CLUBS, THREE),
+                new Card(CLUBS, FOUR),
+                new Card(CLUBS, FIVE),
+                new Card(CLUBS, SIX),
+                new Card(CLUBS, SEVEN),
+                new Card(CLUBS, EIGHT),
+                new Card(CLUBS, NINE),
+                new Card(CLUBS, TEN),
+                new Card(CLUBS, JACK),
+                new Card(CLUBS, QUEEN),
+                new Card(CLUBS, KING),
+                new Card(CLUBS, ACE),
+                new Card(DIAMONDS, TWO),
+                new Card(DIAMONDS, THREE),
+                new Card(DIAMONDS, FOUR),
+                new Card(DIAMONDS, FIVE),
+                new Card(DIAMONDS, SIX),
+                new Card(DIAMONDS, SEVEN),
+                new Card(DIAMONDS, EIGHT),
+                new Card(DIAMONDS, NINE),
+                new Card(DIAMONDS, TEN),
+                new Card(DIAMONDS, JACK),
+                new Card(DIAMONDS, QUEEN),
+                new Card(DIAMONDS, KING),
+                new Card(DIAMONDS, ACE),
+                new Card(HEARTS, TWO),
+                new Card(HEARTS, THREE),
+                new Card(HEARTS, FOUR),
+                new Card(HEARTS, FIVE),
+                new Card(HEARTS, SIX),
+                new Card(HEARTS, SEVEN),
+                new Card(HEARTS, EIGHT),
+                new Card(HEARTS, NINE),
+                new Card(HEARTS, TEN),
+                new Card(HEARTS, JACK),
+                new Card(HEARTS, QUEEN),
+                new Card(HEARTS, KING),
+                new Card(HEARTS, ACE),
+                new Card(SPADES, TWO),
+                new Card(SPADES, THREE),
+                new Card(SPADES, FOUR),
+                new Card(SPADES, FIVE),
+                new Card(SPADES, SIX),
+                new Card(SPADES, SEVEN),
+                new Card(SPADES, EIGHT),
+                new Card(SPADES, NINE),
+                new Card(SPADES, TEN),
+                new Card(SPADES, JACK),
+                new Card(SPADES, QUEEN),
+                new Card(SPADES, KING),
+                new Card(SPADES, ACE));
+    }
+
+    // Other standatd card decks, like 36-card stripped French-Suited deck
+}
