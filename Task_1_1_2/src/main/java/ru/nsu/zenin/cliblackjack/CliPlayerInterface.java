94dd6b1a0013eeb7d91a3cd17b1178b5901558ac
Proletcultist <zenin2606@gmail.com>
2025-09-17 20:34:15 +0700

    [Task_1_1_2] Implemented cli version of blackjack game

---
+++ Task_1_1_2/src/main/java/ru/nsu/zenin/cliblackjack/CliPlayerInterface.java
@@ -0,0 +1,32 @@
+package ru.nsu.zenin.cliblackjack;
+
+import java.util.Scanner;
+import ru.nsu.zenin.cardgame.Message;
+import ru.nsu.zenin.cardgame.PlayerInterface;
+import ru.nsu.zenin.cardgame.Request;
+import ru.nsu.zenin.cardgame.Response;
+
+public class CliPlayerInterface implements PlayerInterface {
+
+    private final Scanner scanner;
+
+    public CliPlayerInterface(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void tell(Message message) {
+        System.out.println(message.getText());
+    }
+
+    public Response ask(Request req) {
+        System.out.println(req.getText());
+
+        String answer = scanner.nextLine();
+
+        return new Response(answer);
+    }
+
+    public void printLinesSeparator() {
+        System.out.println();
+    }
+}
