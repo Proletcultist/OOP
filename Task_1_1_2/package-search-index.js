84a60b07f3ee83ed1dc31da7f4e613cec3dec706
Proletcultist <Proletcultist@users.noreply.github.com>
2025-09-19 14:46:59 +0000

    Deploying to gh-pages from @ Proletcultist/OOP@2d0c6b4754a96cda09df79ad9bcc5054ddee88a0 🚀

---
+++ Task_1_1_2/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.nsu.zenin.blackjack"},{"l":"ru.nsu.zenin.cardgame"},{"l":"ru.nsu.zenin.cardgame.exception"},{"l":"ru.nsu.zenin.cliblackjack"}]
