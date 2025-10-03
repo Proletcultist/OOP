576c71c81725c44f13649ae7563020f77af9181c
Proletcultist <zenin2606@gmail.com>
2025-10-03 13:52:17 +0700

    [Task_1_1_3] Added tests for assignment and expressions

---
+++ Task_1_1_3/src/test/java/ru/nsu/zenin/assignment/AssignmentParserTest.java
@@ -0,0 +1,51 @@
+package ru.nsu.zenin.assignment;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.nsu.zenin.expression.Variable;
+
+class AsignmentParserTest {
+
+    @Test
+    void simpleTest() {
+        Assignment ass = new Assignment();
+
+        ass.bind(new Variable("a"), 100);
+        Assertions.assertEquals(ass.getValue(new Variable("a")), 100);
+    }
+
+    @Test
+    void parseTest() {
+        Assignment ass = AssignmentParser.parse("a = 100; b = 2");
+
+        Assertions.assertEquals(ass.getValue(new Variable("a")), 100);
+        Assertions.assertEquals(ass.getValue(new Variable("b")), 2);
+    }
+
+    @Test
+    void invalidAssignmentTest() {
+        Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    Assignment ass = AssignmentParser.parse("a = = 100; b = 2");
+                });
+
+        Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    Assignment ass = AssignmentParser.parse("a = 100 b = 2");
+                });
+
+        Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    Assignment ass = AssignmentParser.parse("10a = 100; b = 2");
+                });
+
+        Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    Assignment ass = AssignmentParser.parse("a = 100; b = a");
+                });
+    }
+}
