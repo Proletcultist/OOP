09b3a9622d5eae3f4a5c95d6412a97a3826f61a3
Proletcultist <zenin2606@gmail.com>
2025-10-04 12:30:17 +0700

    Removed unneccessary public in interfaces

--- Task_1_1_2/src/main/java/ru/nsu/zenin/cardgame/PlayerInterface.java
+++ Task_1_1_2/src/main/java/ru/nsu/zenin/cardgame/PlayerInterface.java
@@ -2,9 +2,9 @@ package ru.nsu.zenin.cardgame;
 
 public interface PlayerInterface {
 
-    public void tell(Message message);
+    void tell(Message message);
 
-    public Response ask(Request req);
+    Response ask(Request req);
 
-    public void printLinesSeparator();
+    void printLinesSeparator();
 }
