e9bf54487df261c4c8e9367a5900636977cd2638
Proletcultist <zenin2606@gmail.com>
2025-10-04 12:37:54 +0700

    Added GameController class

---
+++ Task_1_1_2/src/main/java/ru/nsu/zenin/cliblackjack/GameController.java
@@ -0,0 +1,29 @@
+package ru.nsu.zenin.cliblackjack;
+
+import java.util.Scanner;
+import ru.nsu.zenin.cardgame.Game;
+
+class GameController {
+
+	private GameController() {}
+
+	static void runGame(Game game, Scanner scanner) {
+		while (true) {
+		    game.startNextRound();
+
+		    System.out.println("Another round? (y/n)");
+
+		    String choice;
+		    while (true) {
+			choice = scanner.nextLine();
+			if (choice.equals("y") || choice.equals("n")) {
+			    break;
+			}
+		    }
+
+		    if (choice.equals("n")) {
+			break;
+		    }
+		}
+	}
+}
