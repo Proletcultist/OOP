286fca5b796382325a216b35b111100adce6115f
Proletcultist <Proletcultist@users.noreply.github.com>
2025-11-07 08:00:24 +0000

    Deploying to gh-pages from @ Proletcultist/OOP@d335b21ee2255c42d453338cd506ac470e4eb39e 🚀

---
+++ Task_1_2_2/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.zenin.hashtable","l":"HashTable"},{"p":"ru.nsu.zenin.util","l":"PrimeIntGenerator"}];updateSearchResults();
