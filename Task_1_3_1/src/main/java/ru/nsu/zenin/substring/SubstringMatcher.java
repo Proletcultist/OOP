4ef2488fc9c805312187fe7c4e0e85ece8bf4d34
Proletcultist <zenin2606@gmail.com>
2025-11-20 18:19:21 +0700

    [Task_1_3_1] Implemented almost whole task (surrogate pairs don't work tho)

---
+++ Task_1_3_1/src/main/java/ru/nsu/zenin/substring/SubstringMatcher.java
@@ -0,0 +1,67 @@
+package ru.nsu.zenin.substring;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class SubstringMatcher implements AutoCloseable {
+    private final SubstringPattern pattern;
+    private final Reader reader;
+
+    private int index = -1;
+    private int k = 0;
+
+    SubstringMatcher(SubstringPattern pattern, Reader reader) {
+        this.pattern = pattern;
+        this.reader = reader;
+    }
+
+    public Optional<Integer> nextMatch() throws IOException {
+        while (true) {
+            int ch = reader.read();
+            index++;
+
+            // If no chars in reader left, return empty
+            if (ch == -1) {
+                break;
+            }
+
+            // Try current char as continuation of previous prefix-suffix
+            // Or try get next smaller prefix-suffix and try again
+            while (k > 0 && (char) ch != pattern.getString().charAt(k)) {
+                k = pattern.getPrefixFunctionValue(k - 1);
+            }
+
+            // If prefix-suffix, to which we can add current char is found, this char prefix suffix
+            // value is k + 1
+            if ((char) ch == pattern.getString().charAt(k)) {
+                k++;
+            }
+
+            if (k == pattern.getString().length()) {
+                k = pattern.getPrefixFunctionValue(k - 1);
+                return Optional.of(index + 1 - pattern.getString().length());
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    public List<Integer> matchAll() throws IOException {
+        List<Integer> out = new ArrayList<Integer>();
+
+        Optional<Integer> next = nextMatch();
+        while (next.isPresent()) {
+            out.add(next.get());
+            next = nextMatch();
+        }
+
+        return out;
+    }
+
+    public void close() throws IOException {
+        reader.close();
+    }
+}
---
+++ Task_1_3_1/src/test/java/ru/nsu/zenin/substring/SubstringMatcherTest.java
@@ -0,0 +1,55 @@
+package ru.nsu.zenin.substring;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SubstringMatcherTest {
+
+    @Test
+    void test() throws IOException {
+        SubstringPattern patt = SubstringPattern.compile("ab");
+        try (SubstringMatcher matcher = patt.matcher(new StringReader("cab"))) {
+            Assertions.assertEquals(matcher.nextMatch(), Optional.of(1));
+        }
+    }
+
+    @Test
+    void test2() throws IOException {
+        SubstringPattern patt = SubstringPattern.compile("abcdabcabcdabcdab");
+        try (SubstringMatcher matcher =
+                patt.matcher(new StringReader("osoabcdabcdabcabcdabcdab"))) {
+            Assertions.assertEquals(matcher.nextMatch(), Optional.of(7));
+            Assertions.assertEquals(matcher.nextMatch(), Optional.empty());
+        }
+    }
+
+    @Test
+    void test3() throws IOException {
+        SubstringPattern patt = SubstringPattern.compile("abc");
+        List<Integer> matches = new ArrayList<Integer>();
+        matches.add(3);
+        matches.add(7);
+        matches.add(12);
+
+        try (SubstringMatcher matcher = patt.matcher(new StringReader("osoabcaabcababc"))) {
+            Assertions.assertEquals(matcher.matchAll(), matches);
+        }
+    }
+
+    @Test
+    void test4() throws IOException {
+        SubstringPattern patt = SubstringPattern.compile("🤣😂");
+        try (SubstringMatcher matcher =
+                patt.matcher(new StringReader("🤣😀🤣😂🤣😂ABOBA!!11!1😂🤣😂"))) {
+            Assertions.assertEquals(matcher.nextMatch(), Optional.of(2));
+            Assertions.assertEquals(matcher.nextMatch(), Optional.of(4));
+            Assertions.assertEquals(matcher.nextMatch(), Optional.of(18));
+            Assertions.assertEquals(matcher.nextMatch(), Optional.empty());
+        }
+    }
+}
