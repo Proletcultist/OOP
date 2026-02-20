81e14c3fc3dbdce3bc0d8d793a23f86018b0ca2c
Proletcultist <zenin2606@gmail.com>
2026-02-20 13:00:04 +0700

    [Task_2_2_1] Add parsing of file provided as argument

--- Task_2_2_1/src/main/java/ru/zenin/Main.java
+++ Task_2_2_1/src/main/java/ru/zenin/Main.java
@@ -1,32 +1,28 @@
 package ru.nsu.zenin;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.IOException;
 import ru.nsu.zenin.pizzeria.dto.PizzeriaDTO;
 import ru.nsu.zenin.pizzeria.mapper.PizzeriaMapper;
 import ru.nsu.zenin.pizzeria.model.Pizzeria;
 
 public class Main {
     public static void main(String[] args) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        String json =
-                """
-                      {
-                          \"cookers\" : [{\"timeToCook\": 20}],
-                          \"deliverers\" : [{\"trunkCapacity\" : 10}],
-                          \"virtualHourValue\" : 123,
-                          \"warehouseCapacity\": 20
-                      }
-                      """;
+        if (args.length != 1) {
+            System.err.println("Wrong amount of arguments, expected: 1, got: " + args.length);
+            return;
+        }
 
-        PizzeriaDTO pizzeriaConfig;
         try {
-            pizzeriaConfig = objectMapper.readValue(json, PizzeriaDTO.class);
+            ObjectMapper objectMapper = new ObjectMapper();
+            PizzeriaDTO pizzeriaConfig =
+                    objectMapper.readValue(new File(args[0]), PizzeriaDTO.class);
             Pizzeria pizzeria = PizzeriaMapper.INSTANCE.fromPizzeriaDTO(pizzeriaConfig);
 
             System.out.println(pizzeria.toString());
-        } catch (JsonProcessingException e) {
-            System.out.println("Failed to read config:\n\t" + e.toString());
+        } catch (IOException e) {
+            System.out.println("Failed to read config:\n\t" + e.getMessage());
         }
     }
 }
