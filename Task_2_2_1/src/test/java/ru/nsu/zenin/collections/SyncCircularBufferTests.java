c8977a4f46c718a6300d90427cb98451336875ab
Proletcultist <zenin2606@gmail.com>
2026-02-20 13:59:49 +0700

    Add test for circular buffer

---
+++ Task_2_2_1/src/test/java/ru/nsu/zenin/collections/SyncCircularBufferTests.java
@@ -0,0 +1,57 @@
+package ru.nsu.zenin.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SyncCircularBufferTest {
+    @Test
+    void fillUpBuffer() throws Exception {
+        SyncCircularBuffer<Integer> buff = new SyncCircularBuffer<Integer>(3);
+
+        Thread writer =
+                new Thread(
+                        () -> {
+                            buff.put(1);
+                            buff.put(2);
+                            buff.put(3);
+
+                            synchronized (buff) {
+                                buff.notify();
+                            }
+
+                            buff.put(4);
+                        });
+        synchronized (buff) {
+            writer.start();
+            buff.wait();
+        }
+
+        List<Integer> readen = new ArrayList<Integer>(3);
+
+        Thread reader =
+                new Thread(
+                        () -> {
+                            for (int i = 0; i < 3; i++) {
+                                readen.add(buff.take());
+                            }
+
+                            synchronized (readen) {
+                                readen.notify();
+                            }
+                        });
+        synchronized (readen) {
+            reader.start();
+            readen.wait();
+        }
+
+        List<Integer> expected = new ArrayList<Integer>(3);
+        expected.add(1);
+        expected.add(2);
+        expected.add(3);
+
+        Assertions.assertEquals(readen, expected);
+        writer.interrupt();
+    }
+}
