562f6d7c13b4ba19d94459f24ffb4843a6cbfa0f
Proletcultist <zenin2606@gmail.com>
2026-03-04 23:36:36 +0700

    [Task_2_2_1] Add blockUntilEmpty and poll methods to BlockingQueue

--- Task_2_2_1/src/main/java/ru/nsu/zenin/collection/BlockingCircularBuffer.java
+++ Task_2_2_1/src/main/java/ru/nsu/zenin/collection/BlockingCircularBuffer.java
@@ -13,14 +13,10 @@ public class BlockingCircularBuffer<T> implements BlockingQueue<T> {
         buffer = (T[]) new Object[capacity + 1];
     }
 
-    public void put(T elem) {
+    public void put(T elem) throws InterruptedException {
         synchronized (write_index) {
             while ((write_index.intValue() + 1) % buffer.length == read_index.intValue()) {
-                try {
-                    write_index.wait();
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                }
+                write_index.wait();
             }
 
             buffer[write_index.intValue()] = elem;
@@ -33,16 +29,41 @@ public class BlockingCircularBuffer<T> implements BlockingQueue<T> {
         }
     }
 
-    public T take() {
+    public T take() throws InterruptedException {
         T ret;
 
         synchronized (read_index) {
             while (read_index.equals(write_index)) {
-                try {
-                    read_index.wait();
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                }
+                read_index.wait();
+            }
+
+            ret = buffer[read_index.intValue()];
+            buffer[read_index.intValue()] = null;
+
+            read_index.setValue((read_index.intValue() + 1) % buffer.length);
+        }
+
+        synchronized (write_index) {
+            write_index.notify();
+        }
+
+        return ret;
+    }
+
+    public void blockUntilEmpty() throws InterruptedException {
+        synchronized (write_index) {
+            while (!read_index.equals(write_index)) {
+                write_index.wait();
+            }
+        }
+    }
+
+    public T poll() {
+        T ret;
+
+        synchronized (read_index) {
+            if (read_index.equals(write_index)) {
+                return null;
             }
 
             ret = buffer[read_index.intValue()];
