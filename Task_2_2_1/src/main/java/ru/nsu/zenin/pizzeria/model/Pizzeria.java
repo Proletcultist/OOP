347d1ca632a5df937d838bb28fa43afa321b7a7e
Proletcultist <zenin2606@gmail.com>
2026-03-05 00:33:24 +0700

    [Task_2_2_1] Implement Pizzeria class

--- Task_2_2_1/src/main/java/ru/nsu/zenin/pizzeria/model/Pizzeria.java
+++ Task_2_2_1/src/main/java/ru/nsu/zenin/pizzeria/model/Pizzeria.java
@@ -1,20 +1,29 @@
 package ru.nsu.zenin.pizzeria.model;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import lombok.AccessLevel;
 import lombok.Getter;
 import ru.nsu.zenin.collection.BlockingCircularBuffer;
 import ru.nsu.zenin.collection.BlockingLinkedList;
 import ru.nsu.zenin.collection.BlockingQueue;
+import ru.nsu.zenin.pizzeria.exception.IllegalPizzeriaStateException;
+import ru.nsu.zenin.pizzeria.exception.NoSuchOrderException;
 
 public class Pizzeria {
     private final List<PizzeriaWorker> workers;
+    private ThreadGroup workerThreads = null;
 
     @Getter(AccessLevel.PACKAGE)
     private final BlockingQueue<Order> warehouse;
 
     @Getter(AccessLevel.PACKAGE)
-    private final BlockingQueue<Order> orders;
+    private final BlockingQueue<Order> pendingOrders = new BlockingLinkedList<Order>();
+
+    private final Map<Integer, Order> orderById = new HashMap<Integer, Order>();
+
+    private int nextOrderId = 0;
 
     @Getter(AccessLevel.PACKAGE)
     private final long virtualHourValue;
@@ -23,7 +32,54 @@ public class Pizzeria {
         this.workers = workers;
         this.virtualHourValue = virtualHourValue;
         this.warehouse = new BlockingCircularBuffer<Order>(warehouseCapacity);
-        this.orders = new BlockingLinkedList<Order>();
+    }
+
+    public synchronized void start() throws IllegalPizzeriaStateException {
+        if (workerThreads != null) {
+            throw new IllegalPizzeriaStateException("Starting already started pizzeria");
+        }
+        nextOrderId = 0;
+        workerThreads = new ThreadGroup("workers");
+        for (PizzeriaWorker worker : workers) {
+            Thread thread = new Thread(workerThreads, worker);
+            thread.start();
+        }
+    }
+
+    public synchronized void stop() throws IllegalPizzeriaStateException {
+        if (workerThreads == null) {
+            throw new IllegalPizzeriaStateException("Stopping already stopped pizzeria");
+        }
+        try {
+            pendingOrders.blockUntilEmpty();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } finally {
+            workerThreads.interrupt();
+            workerThreads = null;
+        }
+    }
+
+    public synchronized int makeOrder(Pizza pizza) throws IllegalPizzeriaStateException {
+        if (workerThreads == null) {
+            throw new IllegalPizzeriaStateException("Cannot make order in closed pizzeria");
+        }
+        try {
+            Order neww = new Order(nextOrderId, pizza);
+            orderById.put(nextOrderId, neww);
+            pendingOrders.put(neww);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        nextOrderId += 1;
+        return nextOrderId - 1;
+    }
+
+    public Order.OrderStatus getOrderStatus(int id) throws NoSuchOrderException {
+        if (!orderById.containsKey(id)) {
+            throw new NoSuchOrderException("No order with id " + id + " in pizzeria");
+        }
+        return orderById.get(id).getStatus();
     }
 
     public void employ(PizzeriaWorker worker) {
