005bbf0c1539016b1569312c8389e80db5f2e706
Proletcultist <zenin2606@gmail.com>
2026-03-05 00:32:30 +0700

    [Task_2_2_1] Enrich Order and OrderStatus classes

--- Task_2_2_1/src/main/java/ru/nsu/zenin/pizzeria/model/Order.java
+++ Task_2_2_1/src/main/java/ru/nsu/zenin/pizzeria/model/Order.java
@@ -1,18 +1,36 @@
 package ru.nsu.zenin.pizzeria.model;
 
+import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 
 @RequiredArgsConstructor
 public class Order {
 
+    private final int id;
     private final Pizza pizza;
-    @Setter private OrderStatus status = OrderStatus.WAITING_FOR_COOKER;
 
+    @Getter(AccessLevel.PACKAGE)
+    private OrderStatus status = OrderStatus.WAITING_FOR_COOKER;
+
+    void setStatus(Order.OrderStatus status) {
+        this.status = status;
+        System.err.println("Order " + this.id + " changed status to: " + this.status);
+    }
+
+    @RequiredArgsConstructor
     public enum OrderStatus {
-        WAITING_FOR_COOKER,
-        COOKING,
-        WAITING_FOR_DELIVERER,
-        IN_DELIVERING
+        WAITING_FOR_COOKER("Waiting for cooker"),
+        COOKING("Cooking"),
+        WAITING_FOR_DELIVERER("Waiting for deliverer"),
+        IN_DELIVERY("In delivery"),
+        DELIVERED("Delivered");
+
+        private final String asString;
+
+        @Override
+        public String toString() {
+            return asString;
+        }
     }
 }
