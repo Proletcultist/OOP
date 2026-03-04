947a4950394d3570ba7bd486cf4ed0a48e0e3bfd
Proletcultist <zenin2606@gmail.com>
2026-03-05 01:22:05 +0700

    [Task_2_2_1] Add logic for work stopping for Cooker and Deliverer

--- Task_2_2_1/src/main/java/ru/nsu/zenin/pizzeria/model/Cooker.java
+++ Task_2_2_1/src/main/java/ru/nsu/zenin/pizzeria/model/Cooker.java
@@ -9,9 +9,16 @@ public class Cooker extends PizzeriaWorker {
     private final long timeToCook;
 
     public void run() {
+        boolean stopToWork = false;
         while (true) {
             try {
-                Order ord = pizzeria.getPendingOrders().take();
+                Order ord = pizzeria.getPendingOrders().poll();
+                if (ord == null && stopToWork) {
+                    return;
+                } else if (ord == null) {
+                    ord = pizzeria.getPendingOrders().take();
+                }
+
                 ord.setStatus(Order.OrderStatus.COOKING);
 
                 TimeUnit.MILLISECONDS.sleep(timeToCook * pizzeria.getVirtualHourValue() / 60);
@@ -19,7 +26,7 @@ public class Cooker extends PizzeriaWorker {
                 ord.setStatus(Order.OrderStatus.WAITING_FOR_DELIVERER);
                 pizzeria.getWarehouse().put(ord);
             } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
+                stopToWork = true;
             }
         }
     }
