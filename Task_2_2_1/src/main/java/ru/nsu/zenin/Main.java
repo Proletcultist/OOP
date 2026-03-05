a980c1c7aec09d7bae804ef3e05f39c29363c588
Proletcultist <zenin2606@gmail.com>
2026-03-06 01:57:36 +0700

    [Task_2_2_1] Implement main

--- Task_2_2_1/src/main/java/ru/nsu/zenin/Main.java
+++ Task_2_2_1/src/main/java/ru/nsu/zenin/Main.java
@@ -3,26 +3,64 @@ package ru.nsu.zenin;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+import ru.nsu.zenin.logging.Logger;
 import ru.nsu.zenin.pizzeria.dto.PizzeriaDTO;
 import ru.nsu.zenin.pizzeria.mapper.PizzeriaMapper;
+import ru.nsu.zenin.pizzeria.model.Pizza;
 import ru.nsu.zenin.pizzeria.model.Pizzeria;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         if (args.length != 1) {
             System.err.println("Wrong amount of arguments, expected: 1, got: " + args.length);
             return;
         }
 
+        PizzeriaDTO pizzeriaConfig;
         try {
             ObjectMapper objectMapper = new ObjectMapper();
-            PizzeriaDTO pizzeriaConfig =
-                    objectMapper.readValue(new File(args[0]), PizzeriaDTO.class);
-            Pizzeria pizzeria = PizzeriaMapper.INSTANCE.fromPizzeriaDTO(pizzeriaConfig);
-
-            System.out.println(pizzeria.toString());
+            pizzeriaConfig = objectMapper.readValue(new File(args[0]), PizzeriaDTO.class);
         } catch (IOException e) {
             System.out.println("Failed to read config:\n\t" + e.getMessage());
+            return;
         }
+
+        Logger.init(System.err);
+
+        Pizzeria pizzeria = PizzeriaMapper.INSTANCE.fromPizzeriaDTO(pizzeriaConfig);
+        pizzeria.start();
+
+        Thread runner =
+                new Thread(
+                        () -> {
+                            try (Scanner scanner = new Scanner(System.in)) {
+                                while (!Thread.currentThread().isInterrupted()) {
+                                    String readen = scanner.next();
+                                    switch (readen) {
+                                        case "peperoni":
+                                            pizzeria.makeOrder(Pizza.PEPERONI);
+                                            break;
+                                        case "margarita":
+                                            pizzeria.makeOrder(Pizza.MARGARITA);
+                                            break;
+                                        default:
+                                            Logger.log(
+                                                    Logger.LogLevel.ERROR,
+                                                    "Unknown pizza: \"" + readen + "\"");
+                                    }
+                                }
+                            }
+                        });
+
+        runner.start();
+        TimeUnit.MILLISECONDS.sleep(pizzeriaConfig.virtualHourValue() * 12);
+        runner.interrupt();
+        runner.join();
+
+        pizzeria.stop();
+
+        Logger.close();
     }
 }
