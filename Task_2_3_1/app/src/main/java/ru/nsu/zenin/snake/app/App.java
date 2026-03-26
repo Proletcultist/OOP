b9df3003846d90a1089d1b8a0b70f6b68e147ff9
Proletcultist <zenin2606@gmail.com>
2026-03-26 11:52:15 +0700

    [Task_2_3_1] Test of javafx

---
+++ Task_2_3_1/app/src/main/java/ru/nsu/zenin/snake/app/App.java
@@ -0,0 +1,22 @@
+package ru.nsu.zenin.snake.app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.xml"));
+        Scene scene = new Scene(root, 640, 480);
+        stage.setTitle("Aboba");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
