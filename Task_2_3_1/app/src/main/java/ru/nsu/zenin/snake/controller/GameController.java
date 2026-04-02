a53e6eaa68dc354ffca30f74ab0f923b88f23815
Proletcultist <zenin2606@gmail.com>
2026-04-02 15:21:53 +0700

    [Task_2_3_1] Fix code style

--- Task_2_3_1/app/src/main/java/ru/nsu/zenin/snake/controller/GameController.java
+++ Task_2_3_1/app/src/main/java/ru/nsu/zenin/snake/controller/GameController.java
@@ -1,17 +1,13 @@
 package ru.nsu.zenin.snake.controller;
 
-import javafx.beans.property.IntegerProperty;
-import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
-import javafx.scene.paint.Color;
-import javafx.collections.FXCollections;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.fxml.FXML;
 import javafx.util.Duration;
 import ru.nsu.zenin.collection.Point2D;
-import ru.nsu.zenin.snake.view.GameFieldView;
-import ru.nsu.zenin.snake.model.Snake;
 import ru.nsu.zenin.snake.model.Game;
+import ru.nsu.zenin.snake.model.Snake;
+import ru.nsu.zenin.snake.view.GameFieldView;
 
 public class GameController {
 
@@ -22,11 +18,15 @@ public class GameController {
     public void initialize() {
         game = new Game(fieldView.getField());
 
-        game.createSnake(new Point2D(0,0), Snake.Direction.RIGHT, 20);
+        game.createSnake(new Point2D(0, 0), Snake.Direction.RIGHT, 20);
 
-        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(50), event -> {
-            game.tick();
-        }));
+        Timeline timeline =
+                new Timeline(
+                        new KeyFrame(
+                                Duration.millis(50),
+                                event -> {
+                                    game.tick();
+                                }));
 
         timeline.setCycleCount(Timeline.INDEFINITE);
 
