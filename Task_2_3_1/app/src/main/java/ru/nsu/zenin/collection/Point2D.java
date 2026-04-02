f35da08425d9904c665e81cfeb1aa0c5a381191d
Proletcultist <zenin2606@gmail.com>
2026-04-02 10:41:40 +0700

    [Task_2_3_1] Add corrrect wrapping of snakes' coords

--- Task_2_3_1/app/src/main/java/ru/nsu/zenin/collection/Point2D.java
+++ Task_2_3_1/app/src/main/java/ru/nsu/zenin/collection/Point2D.java
@@ -1,3 +1,7 @@
 package ru.nsu.zenin.collection;
 
-public record Point2D(int x, int y) {}
+public record Point2D(int x, int y) {
+    public Point2D wrappedAround(int width, int height) {
+        return new Point2D((x() % width + width) % width, (y() % height + height) % height);
+    }
+}
