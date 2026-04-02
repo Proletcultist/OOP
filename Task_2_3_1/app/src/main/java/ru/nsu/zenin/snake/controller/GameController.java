46530ea8f9ee5bcb0bad4d3694f042f785a2adf2
Proletcultist <zenin2606@gmail.com>
2026-04-03 00:50:31 +0700

    [Task_2_3_1] Add score

--- Task_2_3_1/app/src/main/java/ru/nsu/zenin/snake/controller/GameController.java
+++ Task_2_3_1/app/src/main/java/ru/nsu/zenin/snake/controller/GameController.java
@@ -6,6 +6,12 @@ import javafx.fxml.FXML;
 import javafx.util.Duration;
 import javafx.scene.input.KeyCode;
 import javafx.application.Platform;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.scene.text.Font;
+import javafx.beans.binding.Bindings;
+import javafx.geometry.Insets;
 import ru.nsu.zenin.collection.Point2D;
 import ru.nsu.zenin.snake.model.Game;
 import ru.nsu.zenin.snake.model.Snake;
@@ -15,21 +21,58 @@ import ru.nsu.zenin.snake.model.apple.BasicAppleFactory;
 public class GameController {
 
     @FXML private GameFieldView fieldView;
+    @FXML private Text statusBar;
+
+    private int prevScore = 0;
 
     Game game;
 
     public void initialize() {
+        initGraphics();
+        startNewGame();
+        Platform.runLater(() -> fieldView.requestFocus());
+    }
+
+    private void initGraphics() {
+         fieldView.paddingProperty().bind(
+                 Bindings.createObjectBinding(() -> {
+                     double paddingH = fieldView.getWidth() * 0.1;
+                     double paddingV = fieldView.getHeight() * 0.1;
+                     return new Insets(paddingV, paddingH, paddingV, paddingH);
+                 }, fieldView.widthProperty(), fieldView.heightProperty())
+         );
+    }
+
+    private void startNewGame() {
 
         game = new Game(fieldView.getField(), new BasicAppleFactory(), 1);
 
+        prevScore = game.getScore();
+        statusBar.setText("Score: " + game.getScore());
+
         Snake playerSnake = game.createSnake(new Point2D(0, 0), Snake.Direction.RIGHT, 20);
 
-        Timeline timeline =
-                new Timeline(
+        Timeline timeline = new Timeline();
+
+        timeline.getKeyFrames().add(
                         new KeyFrame(
                                 Duration.millis(10),
                                 event -> {
                                     game.tick();
+
+                                    if (game.getScore() != prevScore) {
+                                        statusBar.setText("Score: " + game.getScore());
+                                        prevScore = game.getScore();
+                                    }
+
+                                    if (game.getState() == Game.State.GAME_OVER) {
+                                        timeline.stop();
+                                        statusBar.setText("Game Over! Your score: " + game.getScore() + " (Press R for restart)");
+                                    }
+                                    else if (game.getState() == Game.State.WIN) {
+                                        timeline.stop();
+                                        statusBar.setText("You won! Your score: " + game.getScore() + " (Press R for restart)");
+                                    }
                                 }));
 
         timeline.setCycleCount(Timeline.INDEFINITE);
@@ -44,7 +87,6 @@ public class GameController {
             }
         });
 
-        Platform.runLater(() -> fieldView.requestFocus());
         timeline.play();
     }
 }
