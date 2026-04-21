e05bc0d157afe4361d233e889e523d2f7e26fa37
Proletcultist <zenin2606@gmail.com>
2026-04-21 22:07:08 +0700

    [Task_2_4_1] First steps in implementing dsl

---
+++ Task_2_4_1/src/main/groovy/ru/nsu/zenin/tester/dsl/TaskDelegate.groovy
@@ -0,0 +1,14 @@
+package ru.nsu.zenin.tester.dsl
+
+import ru.nsu.zenin.tester.model.Task
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+class TaskDelegate {
+    private static DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy")
+    List<Task> capturedTasks = []
+
+    def task(int id, String name, int score, String softDeadline, String hardDeadline) {
+        capturedTasks << new Task(id, name, score, LocalDate.parse(softDeadline, dateFormatter), LocalDate.parse(hardDeadline, dateFormatter))
+    }
+}
