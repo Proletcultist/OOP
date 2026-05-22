439ef1f91d19969b5ba93b423a0ee41b89aa9167
Proletcultist <zenin2606@gmail.com>
2026-05-22 14:22:01 +0700

    [Task_2_1_2] Some fixes

--- Task_2_1_2/src/main/java/ru/nsu/zenin/primenumbers/App.java
+++ Task_2_1_2/src/main/java/ru/nsu/zenin/primenumbers/App.java
@@ -1,5 +1,66 @@
 package ru.nsu.zenin.primenumbers;
 
+import java.net.InetAddress;
+import java.util.Arrays;
+
 public class App {
-    public static void main(String[] args) {}
+    public static void main(String[] args) throws Exception {
+        if (args.length < 3) {
+            System.exit(1);
+        }
+
+        String mode = args[0].toLowerCase();
+        String ipStr = args[1];
+        int tcpPort;
+
+        try {
+            tcpPort = Integer.parseInt(args[2]);
+        } catch (NumberFormatException e) {
+            System.err.println("Invalid port number: " + args[2]);
+            return;
+        }
+
+        InetAddress ip = InetAddress.getByName(ipStr);
+        Node node = new Node(ip, tcpPort);
+        node.start();
+
+        System.out.println("Node successfully started on " + ipStr + ":" + tcpPort);
+
+        if ("master".equals(mode)) {
+            runAsMaster(node);
+        } else if ("worker".equals(mode)) {
+            runAsWorker();
+        } else {
+            System.err.println("Unknown mode: '" + mode + "'. Use 'master' or 'worker'.");
+            System.exit(1);
+        }
+    }
+
+    private static void runAsMaster(Node node) throws Exception {
+        int[] testArray = {
+            2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83,
+            89, 97, 100
+        };
+
+        System.out.println("Submitting task to the cluster...");
+        System.out.println("Array: " + Arrays.toString(testArray));
+
+        boolean hasComposite = node.submitTask(testArray);
+
+        System.out.println("--------------------------------------------------");
+        if (hasComposite) {
+            System.out.println("The array contains at least one COMPOSITE number!");
+        } else {
+            System.out.println("The array contains ONLY PRIME numbers!");
+        }
+
+        System.exit(0);
+    }
+
+    private static void runAsWorker() throws InterruptedException {
+        System.out.println("Listening for tasks...");
+        System.out.println("Press Ctrl+C to shut down.");
+
+        Thread.currentThread().join();
+    }
 }
--- Task_2_1_2/src/main/java/ru/nsu/zenin/primenumbers/Connection.java
+++ Task_2_1_2/src/main/java/ru/nsu/zenin/primenumbers/Connection.java
@@ -1,19 +1,28 @@
 package ru.nsu.zenin.primenumbers;
 
+import java.net.InetAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.AsynchronousSocketChannel;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class Connection {
+    private final InetAddress address;
     private final AsynchronousSocketChannel channel;
     private long lastSeen;
-    private List<Task> submittedTasks = new ArrayList<Task>();
+    private final List<Task> submittedTasks = new ArrayList<Task>();
+    private final Queue<ByteBuffer> writeQueue = new ConcurrentLinkedQueue<ByteBuffer>();
+    private final AtomicBoolean isWriting = new AtomicBoolean(false);
 
-    public Connection(AsynchronousSocketChannel channel, long lastSeen) {
+    public Connection(InetAddress address, AsynchronousSocketChannel channel, long lastSeen) {
+        this.address = address;
         this.channel = channel;
         this.lastSeen = lastSeen;
     }
