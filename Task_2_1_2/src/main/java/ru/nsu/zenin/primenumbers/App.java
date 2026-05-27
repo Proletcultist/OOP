b3e865b02b595158e842f7474001ee5076e81077
Proletcultist <zenin2606@gmail.com>
2026-05-27 23:30:23 +0700

    [Task_2_1_2] Add node startup logic

--- Task_2_1_2/src/main/java/ru/nsu/zenin/primenumbers/App.java
+++ Task_2_1_2/src/main/java/ru/nsu/zenin/primenumbers/App.java
@@ -7,28 +7,81 @@ import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.io.Writer;
 import java.net.InetSocketAddress;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import ru.nsu.zenin.primenumbers.logging.Logger;
 
 public class App {
+    static final Pattern ADDRESS_PATTERN = Pattern.compile("(.+):(\\d+)");
+
+    private static InetSocketAddress parseAddr(String str) throws IllegalArgumentException {
+        Matcher m = ADDRESS_PATTERN.matcher(str);
+        if (!m.matches()) {
+            throw new IllegalArgumentException("Invalid format of ip + port string");
+        }
+
+        try {
+            int port = Integer.parseInt(m.group(2));
+            return new InetSocketAddress(m.group(1), port);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Invalid port string");
+        }
+    }
+
     public static void main(String[] args) throws Exception {
-        InetSocketAddress group = new InetSocketAddress("224.0.0.70", 9090);
+        if (args.length < 1) {
+            System.err.println("Required mode. Allowed modes: \"repl\", \"headless\"");
+            System.exit(-1);
+        }
+
+        boolean repl =
+                switch (args[0]) {
+                    case "repl" -> true;
+                    case "headless" -> false;
+                    default -> {
+                        System.err.println("Invaled mode. Allowed modes: \"repl\", \"headless\"");
+                        System.exit(-1);
+                        yield false;
+                    }
+                };
+        InetSocketAddress groupAddress;
+        InetSocketAddress nodeAddress;
+
+        if (args.length < 2) {
+            System.err.println("Required group address. Format: <host>:<port>");
+            System.exit(-1);
+            return;
+        }
+        try {
+            groupAddress = parseAddr(args[1]);
+            if (groupAddress.isUnresolved()) {
+                throw new IllegalArgumentException("Failed to resolve hostname");
+            }
+        } catch (IllegalArgumentException e) {
+            System.err.println("Bad group address: " + e.getMessage());
+            System.exit(-1);
+            return;
+        }
+
+        if (args.length < 3) {
+            System.err.println("Required node address. Format: <host>:<port>");
+            System.exit(-1);
+            return;
+        }
+        try {
+            nodeAddress = parseAddr(args[2]);
+            if (nodeAddress.isUnresolved()) {
+                throw new IllegalArgumentException("Failed to resolve hostname");
+            }
+        } catch (IllegalArgumentException e) {
+            System.err.println("Bad node address: " + e.getMessage());
+            System.exit(-1);
+            return;
+        }
 
         Logger.init(new BufferedWriter(new OutputStreamWriter(System.err)));
-
-        Node b_node1 =
-                new Node(group, new InetSocketAddress("127.0.0.1", 12345)) {
-                    @Override
-                    protected void onShutdown() {}
-                };
-
-        Node b_node2 =
-                new Node(group, new InetSocketAddress("127.0.0.1", 54321)) {
-                    @Override
-                    protected void onShutdown() {}
-                };
-
         Node node =
-                new Node(group, new InetSocketAddress("127.0.0.1", 53321)) {
+                new Node(groupAddress, nodeAddress) {
                     @Override
                     protected void onShutdown() {
                         Logger.close();
@@ -36,12 +89,19 @@ public class App {
                     }
                 };
 
-        try (Reader reader = new BufferedReader(new InputStreamReader(System.in));
-                Writer writer = new OutputStreamWriter(System.out)) {
-            node.serviceRepl(reader, writer);
-        }
+        if (repl) {
+            try (Reader reader = new BufferedReader(new InputStreamReader(System.in));
+                    Writer writer = new OutputStreamWriter(System.out)) {
+                node.serviceRepl(reader, writer);
+            }
 
-        // If repl ended - shutdown node
-        node.shutdown();
+            // If repl ended - shutdown node
+            node.shutdown();
+        } else {
+            // Freeze main thread until process is killed
+            synchronized (node) {
+                node.wait();
+            }
+        }
     }
 }
